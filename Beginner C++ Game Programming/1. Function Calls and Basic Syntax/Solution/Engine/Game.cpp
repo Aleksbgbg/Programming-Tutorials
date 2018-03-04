237dee0d5fff37dc7da919768d6903d01d4f4a85
Aleksbgbg <kozichka01@abv.bg>
2018-03-04 22:08:52 +0000

    Add 'Beginner/1. Function Calls and Basic Syntax/Solution'.

---
+++ Beginner C++ Game Programming/1. Function Calls and Basic Syntax/Solution/Engine/Game.cpp
@@ -0,0 +1,26 @@
+#include "MainWindow.h"
+#include "Game.h"
+
+Game::Game(MainWindow& wnd)
+	:
+	wnd(wnd),
+	gfx(wnd)
+{
+}
+
+void Game::Go()
+{
+	gfx.BeginFrame();
+	UpdateModel();
+	ComposeFrame();
+	gfx.EndFrame();
+}
+
+void Game::UpdateModel()
+{
+}
+
+void Game::ComposeFrame()
+{
+	gfx.PutPixel(100, 100, 255, 255, 255);
+}
