84314eb14417de7bbefa0b455c7c5701186b1669
Aleksbgbg <kozichka01@abv.bg>
2018-03-12 19:04:37 +0000

    Complete solution for tutorial #2 ('Variables and Operators') of 'Beginner'.

--- Beginner C++ Game Programming/2. Variables and Operators/Solution/Engine/Game.cpp
+++ Beginner C++ Game Programming/2. Variables and Operators/Solution/Engine/Game.cpp
@@ -22,19 +22,21 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
-	// Horizontal line
-	gfx.PutPixel(695, 500, 255, 255, 255);
-	gfx.PutPixel(696, 500, 255, 255, 255);
-	gfx.PutPixel(697, 500, 255, 255, 255);
-	gfx.PutPixel(703, 500, 255, 255, 255);
-	gfx.PutPixel(704, 500, 255, 255, 255);
-	gfx.PutPixel(705, 500, 255, 255, 255);
+	//// Horizontal line
+	//gfx.PutPixel(695, 500, 255, 255, 255);
+	//gfx.PutPixel(696, 500, 255, 255, 255);
+	//gfx.PutPixel(697, 500, 255, 255, 255);
+	//gfx.PutPixel(703, 500, 255, 255, 255);
+	//gfx.PutPixel(704, 500, 255, 255, 255);
+	//gfx.PutPixel(705, 500, 255, 255, 255);
+
+	const int x = 5;
 	
 	// Vertical line
-	gfx.PutPixel(700, 495, 255, 255, 255);
-	gfx.PutPixel(700, 496, 255, 255, 255);
-	gfx.PutPixel(700, 497, 255, 255, 255);
-	gfx.PutPixel(700, 503, 255, 255, 255);
-	gfx.PutPixel(700, 504, 255, 255, 255);
-	gfx.PutPixel(700, 505, 255, 255, 255);
+	gfx.PutPixel(x, 495, 255, 255, 255);
+	gfx.PutPixel(x, 496, 255, 255, 255);
+	gfx.PutPixel(x, 497, 255, 255, 255);
+	gfx.PutPixel(x, 503, 255, 255, 255);
+	gfx.PutPixel(x, 504, 255, 255, 255);
+	gfx.PutPixel(x, 505, 255, 255, 255);
 }
