f9c4e054311136808a5c4e6afe6f4848562b2672
Aleksbgbg <kozichka01@abv.bg>
2018-03-13 21:32:34 +0000

    Complete tutorial #3 ('Boolean') of 'Beginner'.

--- Beginner C++ Game Programming/3. Boolean/Solution/Engine/Game.cpp
+++ Beginner C++ Game Programming/3. Boolean/Solution/Engine/Game.cpp
@@ -25,19 +25,25 @@ void Game::ComposeFrame()
 	const int x = 700;
 	const int y = 500;
 
-	// Horizontal line
-	gfx.PutPixel(x - 5, y, 255, 255, 255);
-	gfx.PutPixel(x - 4, y, 255, 255, 255);
-	gfx.PutPixel(x - 3, y, 255, 255, 255);
-	gfx.PutPixel(x + 3, y, 255, 255, 255);
-	gfx.PutPixel(x + 4, y, 255, 255, 255);
-	gfx.PutPixel(x + 5, y, 255, 255, 255);
-	
-	// Vertical line
-	gfx.PutPixel(x, y - 5, 255, 255, 255);
-	gfx.PutPixel(x, y - 4, 255, 255, 255);
-	gfx.PutPixel(x, y - 3, 255, 255, 255);
-	gfx.PutPixel(x, y + 3, 255, 255, 255);
-	gfx.PutPixel(x, y + 4, 255, 255, 255);
-	gfx.PutPixel(x, y + 5, 255, 255, 255);
+	if (wnd.kbd.KeyIsPressed(VK_UP))
+	{
+		if (wnd.kbd.KeyIsPressed(VK_DOWN))
+		{
+			// Horizontal line
+			gfx.PutPixel(x - 5, y, 255, 255, 255);
+			gfx.PutPixel(x - 4, y, 255, 255, 255);
+			gfx.PutPixel(x - 3, y, 255, 255, 255);
+			gfx.PutPixel(x + 3, y, 255, 255, 255);
+			gfx.PutPixel(x + 4, y, 255, 255, 255);
+			gfx.PutPixel(x + 5, y, 255, 255, 255);
+
+			// Vertical line
+			gfx.PutPixel(x, y - 5, 255, 255, 255);
+			gfx.PutPixel(x, y - 4, 255, 255, 255);
+			gfx.PutPixel(x, y - 3, 255, 255, 255);
+			gfx.PutPixel(x, y + 3, 255, 255, 255);
+			gfx.PutPixel(x, y + 4, 255, 255, 255);
+			gfx.PutPixel(x, y + 5, 255, 255, 255);
+		}
+	}
 }
